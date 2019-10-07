c000328c54bbd516295fa4d129b4e66d24b479e9
Modzel <hootwheels989@gmail.com>
2019-10-07 22:53:54 +0200

    Przycisk dodaj otwiera okno dialogowe

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -12,17 +12,40 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.IO;
+using System.Media;
+using Microsoft.Win32;
 namespace WpfApp1
 {
     /// <summary>
     /// Logika interakcji dla klasy MainWindow.xaml
     /// </summary>
+
+
     public partial class MainWindow : Window
     {
+        public string filePath;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Dodaj_Click(object sender, RoutedEventArgs e)
+        {
+            
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+
+            }
+
+           
         }
     }
 }
